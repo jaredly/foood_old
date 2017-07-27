4dfea72722079dda8f7d8a773f6faaf8f7c0fad1
Jared Forsyth <jared@jaredforsyth.com>
2017-07-26 23:17:51 -0600

    better imports

--- server/src/importer.js
+++ server/src/importer.js
@@ -11,18 +11,6 @@ const zz = `
 "image"
 
 //
-"name"
-"description"
-"ingredients"
-"ingredients"
-"recipeInstructions"
-"recipeInstructions"
-"recipeInstructions"
-"totalTime"
-"prepTime"
-"recipeYield"
-
-//
 "nutrition"
   "calories"
   "carbohydrateContent"
@@ -58,14 +46,78 @@ const unwhite = text => {
   return text.replace(/\s+/g, ' ').trim()
 }
 
+const units = {
+  'cup': ['C', 'c', 'cup', 'cups'],
+  'tablespoon': ['T', 'Tbs', 'tbs', 'tablespoon', 'tablespoons', 'Tablespoon', 'Tablespoons'],
+  'teaspoon': ['t', 'tsp', 'Tsp', 'teaspoon', 'teaspoons', 'Teaspoon', 'Teaspoons'],
+  'ounce': ['oz', 'ounce', 'ounces'],
+  'gram': ['g', 'gram', 'G', 'Gram', 'grams', 'Grams'],
+  'kilogram': ['kg', 'Kg', 'Kilogram', 'Kilograms', 'kilogram', 'kilograms'],
+  'quart': ['quart', 'Quart', 'quarts', 'qts', 'qt'],
+  'can': ['can', 'cans', 'Can', 'Cans'],
+  'package': ['package', 'Package', 'pkg', 'Pkg'],
+}
+
+const parseUnit = (amount, text) => {
+  text = text.trim()
+  for (let unit in units) {
+    for (let n in units[unit]) {
+      if (text.indexOf(unit) === 0) {
+        return {
+          amount,
+          unit,
+          rest: text.slice(unit.length).trim(),
+        }
+      }
+    }
+  }
+
+  return {
+    amount,
+    unit: null,
+    rest: text.trim(),
+  }
+}
+
+const parseIngredient = text => {
+  const numberMatch = text.match(/^(\d+)\s+(\d)\/(\d)/)
+  if (numberMatch) {
+    return parseUnit(
+      +numberMatch[1] + (+numberMatch[2] / +numberMatch[3]),
+      text.slice(numberMatch[0].length)
+    )
+  }
+  const fractionMatch = text.match(/^(\d+)\/(\d+)/)
+  if (fractionMatch) {
+    return parseUnit(
+      +fractionMatch[1] / +fractionMatch[2],
+      text.slice(fractionMatch[0].length)
+    )
+  }
+  const singleMatch = text.match(/^(\d+)/)
+  if (singleMatch) {
+    return parseUnit(
+      +singleMatch[1],
+      text.slice(singleMatch[0].length)
+    )
+  }
+  return {
+    amount: null,
+    unit: null,
+    rest: text.trim(),
+  }
+}
+
 export default text => {
   const $ = cheerio.load(text)
-  const name = unwhite($('[itemprop=name]').text())
+  const title = unwhite($('[itemprop=name]').text())
   const description = unwhite($('[itemprop=description]').text())
   const ingredients = $('[itemprop=ingredients]')
     .map((i, elem) => $(elem).text())
     .get()
     .map(unwhite)
+    .map(parseIngredient)
+
   const allRecipeInstructions = $('[itemprop=recipeInstructions]')
     .map((i, elem) => {
       const dt = $(elem).children('dt').get()
@@ -75,16 +127,18 @@ export default text => {
       return [$(elem).text()]
     })
     .get()
-  const recipeInstructions = [].concat(...allRecipeInstructions).map(unwhite)
+
+  const instructions = [].concat(...allRecipeInstructions).map(unwhite)
   // TODO nutrition
   const totalTime = getTime($('[itemprop=totalTime]'))
   const prepTime = getTime($('[itemprop=prepTime]'))
   const cookTime = getTime($('[itemprop=cookTime]'))
+
   return {
-    name,
+    title,
     description,
     ingredients,
-    recipeInstructions,
+    instructions,
     totalTime,
     prepTime,
     cookTime,
